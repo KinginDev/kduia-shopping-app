4af95cfead2c6e696c81b6eedcd9f835a5151c93
sleeping <you@example.com>
2023-06-25 04:37:39 +0100

    Cart App

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,30 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseItem from './ExpenseItem';
 
 const ExpenseList = () => {
+    const {expenses} = useContext(AppContext)
+
+    return (
+        <div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th scope='col'>Items</th>
+                        <th scope='col'>Quantity</th>
+                        <th scope='col'>Unit Price</th>
+                        <th scope='col'>Items Price</th>
+                        <th scope='col'>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {expenses.map((expense) => (
+                    <ExpenseItem name={expense.name} quantity={expense.quantity} id={expense.id} key={expense.id} unitprice={expense.unitprice} />
+                   ))}
+                </tbody>
+            </table>
+        </div>
+    )
 };
 
 export default ExpenseList;
